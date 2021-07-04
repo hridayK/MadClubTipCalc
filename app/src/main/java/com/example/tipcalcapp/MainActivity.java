bb43e8bab783cf23506a0fee2233514a8dfc8b40
Hriday Keswani <hridaykeswani@gmail.com>
2021-07-04 12:16:17 +0530

    added slider functionality and created the second activity

--- app/src/main/java/com/example/tipcalcapp/MainActivity.java
+++ app/src/main/java/com/example/tipcalcapp/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.tipcalcapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText amount;
+    private TextView percentage;
+    private SeekBar SetPercentage;
+
+    private int per,amt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SetPercentage = (SeekBar) findViewById(R.id.slidr);
+        amount = (EditText) findViewById(R.id.amt);
+        percentage = (TextView) findViewById(R.id.percentage);
+
+        SetPercentage.setMax(10);
+
+        SetPercentage.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                percentage.setText(progress+"%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void ChangeActivity(View view){
+        
     }
 }
---
+++ app/src/main/java/com/example/tipcalcapp/Tip.java
@@ -0,0 +1,14 @@
+package com.example.tipcalcapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip);
+    }
+}
